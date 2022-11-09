2a34d95d87e6a7ba60c5dd7233adeb0495c97256
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-11-09 22:59:07 +0100

    Start to work on shopping list Add item

--- My-First-Training-Angular-Project/src/app/shopping-list/shopping-list.component.ts
+++ My-First-Training-Angular-Project/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.module';
+import { Ingredient } from '../../../shared/ingredient.module';
 
 @Component({
   selector: 'app-shopping-list',
@@ -14,6 +14,10 @@ export class ShoppingListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
   }
 
 }
