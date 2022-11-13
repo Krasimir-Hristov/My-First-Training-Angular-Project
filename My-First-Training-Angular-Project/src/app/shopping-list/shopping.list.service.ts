f379cd7cf4cf1c1b18ee5855256f3faa7e30bde0
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-11-13 19:57:38 +0100

    CRUD for Shopping List
    Create the CRUD funcionality for the Shopping List

--- My-First-Training-Angular-Project/src/app/shopping-list/shopping.list.service.ts
+++ My-First-Training-Angular-Project/src/app/shopping-list/shopping.list.service.ts
@@ -4,6 +4,7 @@ import { Ingredient } from "src/shared/ingredient.model";
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
@@ -12,6 +13,10 @@ export class ShoppingListService {
     getIngredients() {
         return this.ingredients.slice();
     } 
+
+    getInredient(index: number) {
+        return this.ingredients[index];
+    }
     
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
@@ -20,6 +25,16 @@ export class ShoppingListService {
 
     addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients);
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1);
         this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
