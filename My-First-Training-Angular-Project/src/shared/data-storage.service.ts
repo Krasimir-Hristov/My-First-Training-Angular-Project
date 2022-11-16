93f28df84c66d022d07da6189eccb657feb79a5a
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-11-16 20:30:52 +0100

    Adding the Token with an Interceptor

--- My-First-Training-Angular-Project/src/shared/data-storage.service.ts
+++ My-First-Training-Angular-Project/src/shared/data-storage.service.ts
@@ -1,14 +1,15 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map, tap } from "rxjs";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { exhaustMap, map, take, tap } from "rxjs";
 
 import { Recipe } from "src/app/recipes/recipe.model";
+import { AuthService } from "src/app/auth/auth.service";
 import { RecipeService } from "src/app/recipes/recipe.service";
 
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
@@ -20,10 +21,14 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.http
-        .get<Recipe[]>('https://ng-training-project-recipes-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
-        .pipe(
-            map(recipes => {
+            return this.authService.user.pipe(take(1), exhaustMap(user => {
+            return this.http
+            .get<Recipe[]>('https://ng-training-project-recipes-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', 
+            {
+                params: new HttpParams().set('auth', user.token)
+            });
+        }),
+        map(recipes => {
             return recipes.map(recipe => {
                 return {
                     ...recipe, 
@@ -34,7 +39,8 @@ export class DataStorageService {
         tap(recipes => {
             this.recipeService.setRecipes(recipes);
         })
-        )
-   
+        );
     }
 }
+      
+   
