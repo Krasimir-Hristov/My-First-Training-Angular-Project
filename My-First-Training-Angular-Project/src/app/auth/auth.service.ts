27465160a57b5faae42e570cd098b4f6d731c40a
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-11-16 17:26:19 +0100

    Sending the Signup Request

---
+++ My-First-Training-Angular-Project/src/app/auth/auth.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+interface AuthResponseData {
+    idToken: string,
+    email: string,
+    refreshToken: string,
+    expiresIn: string,
+    localId: string
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+    constructor(private http: HttpClient) {}
+
+    signUp(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCSwJZIck_IcM-lfFb0iehl861Hh4-_gWw',
+        {
+           email: email,
+           password: password,
+           returnSecureToken: true
+        } );
+    }
+}
