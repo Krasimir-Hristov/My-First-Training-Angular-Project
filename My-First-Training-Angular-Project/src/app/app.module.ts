3e0c76fbefeecf483825cd3356d1fd925c4ab9bf
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-11-17 20:28:25 +0100

    Core Module

--- My-First-Training-Angular-Project/src/app/app.module.ts
+++ My-First-Training-Angular-Project/src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListService } from './shopping-list/shopping.list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from 'src/shared/data-storage.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterseptorService } from './auth/auth-interceptor.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from 'src/shared/shared.Module';
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [
@@ -29,9 +26,9 @@ import { SharedModule } from 'src/shared/shared.Module';
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterseptorService, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
