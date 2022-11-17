a442bd9bdf8873a6c54e4d1815e72edb7d2373f1
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-11-17 20:39:12 +0100

    Adding Auth Module

--- My-First-Training-Angular-Project/src/app/app.module.ts
+++ My-First-Training-Angular-Project/src/app/app.module.ts
@@ -1,31 +1,28 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from 'src/shared/shared.Module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/Auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
+    AuthModule,
     SharedModule,
     CoreModule
   ],
