fd6303d95db838daef0ff74ee6e7375368d24299
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-11-17 21:17:25 +0100

    Adding preload Lazy code

--- My-First-Training-Angular-Project/src/app/app-routing.module.ts
+++ My-First-Training-Angular-Project/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
 
 
 
@@ -20,7 +20,8 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })
+    ],
     exports: [RouterModule]
 })
 
